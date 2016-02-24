edbb4dae20e18c2f44a1719e73c4f379e026a66b
Todd Benning <todd.benning@breakawaydev.net>
2016-02-24 09:20:59 -0600

    Improved build responder and added one for pull requests

---
+++ CsAlertClient/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CsAlertClient
+{
+    using System.Linq;
+    using System.Speech.Synthesis;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var synth = new SpeechSynthesizer();
+            var voice = synth.GetInstalledVoices()
+                .FirstOrDefault(v => v.VoiceInfo.Gender == VoiceGender.Female);
+
+            synth.SelectVoiceByHints(VoiceGender.Female);
+            synth.Speak("Hello world!");
+        }
+    }
+}
--- NodeClient/app.js
+++ NodeClient/app.js
@@ -6,7 +6,7 @@ function checkForMessages(sbService, queueName, callback) {
     sbService.receiveQueueMessage(queueName, { isPeekLock: true }, function (err, lockedMessage) {
         if (err) {
             if (err === "No messages to receive") {
-                console.log("No messages");
+                //console.log("No messages");
             } else {
                 callback(err);
             }
@@ -16,25 +16,33 @@ function checkForMessages(sbService, queueName, callback) {
     });
 }
 
-function tellItLikeItIs(message) {
+function speakMessage(message) {
     //say.speak(message);
-    winsay.speak(null, message, function(error, result) {
+    winsay.speak("Red", message, function(error, result) {
         if (error) {
-            console.log("Failed to tell it like it is ", error);
+            console.log("Failed to speak the message '" + message + "'\nResult: " + result, error);
         }
     });
 }
 
 function processBuildCompleted(build) {
 
-    var res = build.resource;
-    if (res.status !== "failed") {
-        return;
+    var name = build.requestedFor.displayName;
+
+    if (build.result === "succeeded") {
+        speakMessage("Hey everybody! " + name + " finally checked something in that didn't break the build! Yay!")
     }
+    else if (build.result === "failed") {
+        var msg = name + ", that code you just built is probably the most insanely idiotic code I have ever seen. At no point in your scattered, incoherent logic were you even close to anything that could be considered a decent application. Everyone in this room is now dumber for having seen it. I award you no points, and may God have mercy on your soul."
+        speakMessage(msg);
+    }
+}
 
-    var name = res.requests[0].requestedFor.displayName;
+function processPullRequest(pullRequest) {
 
-    tellItLikeItIs("God dammit " + name + "! You broke the build again.");
+    var name = pullRequest.createdBy.displayName;
+
+    speakMessage("Everyone drop everything! " + name + " wants you to review what is sure to be the greatest code ever written. Probably.")
 }
 
 function processMessage(sbService, err, lockedMsg) {
@@ -48,11 +56,22 @@ function processMessage(sbService, err, lockedMsg) {
         }
     } else {
         console.log("Rx: ", lockedMsg);
-        processBuildCompleted(JSON.parse(lockedMsg.body));
+
+        try {
+            var message = JSON.parse(lockedMsg.body);
+            if (message.eventType === "build.complete") {
+                processBuildCompleted(message.resource);
+            }
+            else if (message.eventType === "git.pullrequest.created") {
+                processPullRequest(message.resource);
+            }
+        } catch (e) {
+            console.log("Error parsing service bus message: " + e);
+        }
+
         sbService.deleteMessage(lockedMsg, function (err2) {
             if (err2) {
                 console.log("Failed to delete message: ", err2);
-
             } else {
                 console.log("Deleted message.");
             }
@@ -60,6 +79,7 @@ function processMessage(sbService, err, lockedMsg) {
     }
 }
 
+// Sends a service bus message. TODO: Move to tests
 var idx = 0;
 function sendMessages(sbService, queueName) {
     var msg = "Message # " + (++idx);
@@ -72,11 +92,14 @@ function sendMessages(sbService, queueName) {
     });
 }
 
-var connStr = process.argv[2] || "Endpoint=sb://buildalerts.servicebus.windows.net/;SharedAccessKeyName=FullAccess;SharedAccessKey=dnAjDUay6qn/x9X06dJbbz1F4O5dBJ0GcV86BTj9O5g=";
-if (!connStr) throw new Error("Must provide connection string");
-var queueName = "failedbuilds";
+var queueName = process.argv[2];
+if (!queueName) throw new Error("Must provide queue name");
 
-winsay.speak(null, "Hello. Let's get started.");
+var connStr = process.argv[3];
+if (!connStr) throw new Error("Must provide connection string");
+
+console.log("Build Alert System online. Waiting for messages...");
+//speakMessage("Build Alert System online. Waiting for messages...");
 
 console.log("Connecting to " + connStr + " queue " + queueName);
 var sbService = azure.createServiceBusService(connStr);
@@ -85,6 +108,5 @@ sbService.createQueueIfNotExists(queueName, function (err) {
         console.log("Failed to create queue: ", err);
     } else {
         setInterval(checkForMessages.bind(null, sbService, queueName, processMessage.bind(null, sbService)), 5000);
-        //setInterval(sendMessages.bind(null, sbService, queueName), 15000);
     }
 });
